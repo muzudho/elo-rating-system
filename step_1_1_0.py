5bacd5eca73d5881574f977c8aae088d3e088f58
むずでょ <muzudho1@gmail.com>
2023-12-11 21:45:01 +0900

    step_1_1_0.py

--- step_1_1_0.py
+++ step_1_1_0.py
@@ -1,31 +1,38 @@
 #
-# python step_1_0.py
+# python step_1_1_0.py
 #
 import random
+
+counts = [0,0,0]
 
 # 0, 1, 2 のいずれかを返す
 def gyanken():
     return random.randint(0, 2)
 
-if __name__ == "__main__":
+def on_result_1(result):
+    counts[result] += 1
 
+def main(on_result):
     print("Please input round number(1-100):")
     round = int(input())
 
-    counts = [0,0,0]
 
     for i in range(0, round):
-        result = gyanken()
+        result_1 = gyanken()
 
-        counts[result] += 1
+        on_result(result_1)
 
-        if result == 0:
+        if result_1 == 0:
             print("aiko")
-        elif result == 1:
+        elif result_1 == 1:
             print("A win")
-        elif result == 2:
+        elif result_1 == 2:
             print("B win")
         else:
             print("Error")
 
     print(f"aiko: {counts[0]}, A win: {counts[1]}, B win: {counts[2]}")
+
+
+if __name__ == "__main__":
+    main(on_result=on_result_1)
