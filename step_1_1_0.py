dc9f69eea0727425e60992118895d1bf0775155f
むずでょ <muzudho1@gmail.com>
2023-12-11 21:49:24 +0900

    Update step_1_1_0.py

--- step_1_1_0.py
+++ step_1_1_0.py
@@ -5,14 +5,10 @@ import random
 
 counts = [0,0,0]
 
-# 0, 1, 2 のいずれかを返す
-def gyanken():
-    return random.randint(0, 2)
 
-def on_result_1(result):
-    counts[result] += 1
-
-def main(on_result):
+def main(
+        on_result,
+        on_end):
     print("Please input round number(1-100):")
     round = int(input())
 
@@ -20,19 +16,34 @@ def main(on_result):
     for i in range(0, round):
         result_1 = gyanken()
 
+        counts[result_1] += 1
+
         on_result(result_1)
 
-        if result_1 == 0:
+    on_end()
+
+
+if __name__ == "__main__":
+
+    # 0, 1, 2 のいずれかを返す
+    def gyanken():
+        return random.randint(0, 2)
+
+
+    def on_result_1(result):
+        if result == 0:
             print("aiko")
-        elif result_1 == 1:
+        elif result == 1:
             print("A win")
-        elif result_1 == 2:
+        elif result == 2:
             print("B win")
         else:
             print("Error")
 
-    print(f"aiko: {counts[0]}, A win: {counts[1]}, B win: {counts[2]}")
+
+    def on_end_1():
+        print(f"aiko: {counts[0]}, A win: {counts[1]}, B win: {counts[2]}")
 
 
-if __name__ == "__main__":
-    main(on_result=on_result_1)
+    main(on_result=on_result_1,
+         on_end=on_end_1)
