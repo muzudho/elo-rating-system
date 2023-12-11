cd95230f20d4cfe7c12a1c01f92398328c9a310b
むずでょ <muzudho1@gmail.com>
2023-12-11 23:55:59 +0900

    logarithm

--- step_2_0.py
+++ step_2_0.py
@@ -1,7 +1,7 @@
 #
 # python step_1_1_0.py
 #
-import random
+import math
 
 from step_1_1_0 import on_gyanken_1, games, main
 
@@ -10,6 +10,30 @@ ratings = [0, 2000, 2000]
 
 # Constant K
 K = 32
+
+
+# Rating calculate
+def get_x_by_y(y):
+    if y==0:
+        return 1
+    else:
+        return 400 * math.log10(y)
+
+
+# Rating calculate
+def get_y_by_x(x):
+    return x ** (x / 400)
+
+
+# Win rate
+def get_win_rate_for_upper_rating(win_games):
+    return win_games / (win_games + 1)
+
+
+# Win rate
+def get_win_rate_for_lower_rating(win_games):
+    return 1 / (win_games + 1)
+
 
 if __name__ == "__main__":
 
@@ -20,11 +44,13 @@ if __name__ == "__main__":
         ab = ratings[1] - ratings[2]
         ba = ratings[2] - ratings[1]
         y = abs(ba)
+        print(f"y: {y}")
+        x = get_x_by_y(y)
 
 
         # 勝率
-        wab = ab
-        wba = ba
+        win_rate_for_lower_rating = 1 / (x+1)
+        #win_rate_for_upper_rating = x / (x+1)
 
 
         if result == 0:
@@ -32,13 +58,13 @@ if __name__ == "__main__":
             print(f"aiko  >  ratings A {ratings[1]}, B {ratings[2]}")
 
         elif result == 1:
-            offset = K * wba
+            offset = K * win_rate_for_lower_rating
             ratings[1] += offset
             ratings[2] -= offset
             print(f"A win  >  ratings A {ratings[1]}, B {ratings[2]}")
 
         elif result == 2:
-            offset = K * wab
+            offset = K * win_rate_for_lower_rating
             ratings[2] += offset
             ratings[1] -= offset
             print(f"B win  >  ratings A {ratings[1]}, B {ratings[2]}")
