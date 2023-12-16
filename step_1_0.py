5a2a486e1589719fb01f883928fe270e0fae49b2
むずでょ <muzudho1@gmail.com>
2023-12-16 14:52:25 +0900

    WIP 構造化中

--- step_1_0.py
+++ step_1_0.py
@@ -3,29 +3,53 @@
 #
 import random
 
-# 0, 1, 2 のいずれかを返す
+
+# 0: あいこ, 1: プレイヤー１の勝ち, 2: プレイヤー２の勝ち のいずれかを返す
 def gyanken():
     return random.randint(0, 2)
 
-if __name__ == "__main__":
+
+def main(
+        on_tournament_executed):
+    """プログラムの全体"""
 
     print("Please input round number(1-100):")
     round = int(input())
 
-    counts = [0,0,0]
+    # 大会の実行
+    on_tournament_executed(round)
 
-    for i in range(0, round):
-        result = gyanken()
 
-        counts[result] += 1
+if __name__ == "__main__":
 
-        if result == 0:
-            print("aiko")
-        elif result == 1:
-            print("A win")
-        elif result == 2:
-            print("B win")
-        else:
-            print("Error")
+    def on_my_tournament_executed(round):
+        """大会を実行する
 
-    print(f"aiko: {counts[0]}, A win: {counts[1]}, B win: {counts[2]}")
+        Parameters
+        ----------
+        round : int
+            対局数
+        """
+
+        counts = [0,0,0]
+
+        for i in range(0, round):
+            result = gyanken()
+
+            counts[result] += 1
+
+            if result == 0:
+                print("aiko")
+            elif result == 1:
+                print("A win")
+            elif result == 2:
+                print("B win")
+            else:
+                print("Error")
+
+        print(f"aiko: {counts[0]}, A win: {counts[1]}, B win: {counts[2]}")
+
+
+    # プログラムの実行
+    main(
+        on_tournament_executed = on_my_tournament_executed)
