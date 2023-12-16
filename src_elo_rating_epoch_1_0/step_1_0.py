67fc4940e1783cb573dac7be6328609e0da95299
むずでょ <muzudho1@gmail.com>
2023-12-17 01:15:52 +0900

    epoch 1.0

---
+++ src_elo_rating_epoch_1_0/step_1_0.py
@@ -0,0 +1,99 @@
+#
+# cd src_elo_rating_epoch_1_0
+#
+# このファイルはライブラリなので、実行しても何もしません
+#
+import math
+
+def calculate_moving_rating_that_a_wins(K, ratings):
+    """A が勝った時のレーティングの移動量
+
+    Parameters
+    ----------
+    K : int
+        定数Ｋ。一般的に 32 とか 16 とかの整数
+    ratings : list
+        レーティング。 [0]未使用, [1]先手プレイヤーのレーティング, [2]後手プレイヤーのレーティング
+    """
+
+    result = {}
+
+    # b から見た a とのレーティング差
+    result["difference_b_to_a"] = ratings[1] - ratings[2]
+
+    # b から見た a に１勝するために必要な対局数
+    result["games_b_to_a"] = get_games_by_rating_difference(result["difference_b_to_a"])
+
+    # b から見た a への勝率
+    if 0 <= result["difference_b_to_a"]:
+        result["Wba"] = get_win_rate_for_upper_rating(result["games_b_to_a"])
+    else:
+        result["Wba"] = get_win_rate_for_lower_rating(result["games_b_to_a"])
+
+    # レーティングの移動量
+    result["moving_rating"] = math.floor(K * result["Wba"])
+    return result
+
+def calculate_moving_rating_that_b_wins(K, ratings):
+    """B が勝った時のレーティングの移動量
+
+    Parameters
+    ----------
+    K : int
+        定数Ｋ。一般的に 32 とか 16 とかの整数
+    ratings : list
+        レーティング。 [0]未使用, [1]先手プレイヤーのレーティング, [2]後手プレイヤーのレーティング
+    """
+
+    result = {}
+
+    # a から見た b とのレーティング差
+    result["difference_a_to_b"] = ratings[2] - ratings[1]
+
+    # a から見た b に１勝するために必要な対局数
+    result["games_a_to_b"] = get_games_by_rating_difference(result["difference_a_to_b"])
+
+    # a から見た b への勝率
+    if 0 <= result["difference_a_to_b"]:
+        result["Wab"] = get_win_rate_for_upper_rating(result["games_a_to_b"])
+    else:
+        result["Wab"] = get_win_rate_for_lower_rating(result["games_a_to_b"])
+
+    # レーティングの移動量
+    result["moving_rating"] = math.floor(K * result["Wab"])
+    return result
+
+
+def get_games_by_rating_difference(
+        rating_difference):
+    """１勝するために必要な対局数（暗記表の x ）を取得
+    実数でも算出できるが、（1.0 以上の数になるよう数式を調整している前提で）整数にして返す
+
+    Parameters
+    ----------
+    rating_difference : int
+        暗記表の y
+    """
+    return math.floor(10 ** (rating_difference / 400))
+
+
+def get_rating_difference_by_games(
+        games_number):
+    """レーティング差（暗記表の y ）を取得
+
+    Parameters
+    ----------
+    games_number : float
+        暗記表の x : 実数
+    """
+    return math.floor(400 * math.log10(games_number))
+
+
+def get_win_rate_for_upper_rating(win_games):
+    """Win rate : 実数"""
+    return win_games / (win_games + 1)
+
+
+def get_win_rate_for_lower_rating(win_games):
+    """Win rate : 実数"""
+    return 1 / (win_games + 1)
